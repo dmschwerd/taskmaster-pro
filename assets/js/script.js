6dfc0c23755753b9fad3314f1c177255b64c50a1
David <dmsch656@gmail.com>
2023-03-24 15:05:57 -0700

    editable dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,9 +55,10 @@ $(".list-group").on("click", "p", function() {
     .val(text);
 
   $(this).replaceWith(textInput);
-  testIput.trigger("focus")
+  textInput.trigger("focus")
 });
 
+// value of list item was changed
 $(".list-group").on("blur", "textarea", function() {
   // get texarea's updated value
   var text = $(this)
@@ -87,6 +88,55 @@ $(".list-group").on("blur", "textarea", function() {
   $(this).replaceWith(taskP);
 });
 
+// on click of a <span> element with the "list-group" class focus on and allow editing
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and resave to localStorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
